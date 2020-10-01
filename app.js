02a955bf09ed2597668a2696fd80050d3b664b6e
SBuchanan1 <syd.buch@gmail.com>
2020-09-30 22:01:58 -0400

    added classes and constructors

--- app.js
+++ app.js
@@ -33,3 +33,9 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+function getTeamMembers() {
+    console.log("Create your employee!")
+    inquirer.prompt
+}
+getTeamMembers()
