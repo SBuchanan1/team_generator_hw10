46e9ec9e4c0b7c17bd3165c92638e28634c3bded
SBuchanan1 <syd.buch@gmail.com>
2020-10-01 01:01:53 -0400

    final changes to the app js. all tests are passing

--- app.js
+++ app.js
@@ -47,6 +47,76 @@ function promptCommon(userInput) {
 
     }
 }
+// The above curly brace marks the end of the prompt
+
+// Prompt based on users response
+function specialPrompt(userResponse){
+   if(userResponse.employeeRole === "Manager") {
+       inquirer.prompt {[
+           {
+               name: "officeNumber",
+               type: "input",
+               message: "Please enter office number: ",
+           }
+       ]).then(function(specialResponse) {
+           const manager = new Manager(userResponse.employeeName, userResponse.employeeId, userResponse.employeeEmail, specialResponse.officeNumber);
+           employeeArray.push.(manager);
+           stopPrompt();
+        }).catch(function(err){
+            if(err) throw err
+        })
+   }else if (userResponse.employeeRole === "Engineer") {
+       inquirer.prompt {[
+           {
+               name: "github",
+               type: "input",
+               message: "Please enter your Github username: "
+           }
+       ]).then(function(specialResponse){
+           const engineer = new Engineer(userResponse.employeeName, userResponse.employeeId, userResponse.employeeEmail, specialResponse.github)
+           employeeArray.push(engineer);
+           stopPrompt();
+       }).catch(function(err){
+           if(err) throw err
+       })
+    } else {
+        inquirer.prompt {[
+            {
+                name: "school",
+                type: "input",
+                message: "Where did the employee attend school?"
+            }
+        ]}.then(function(specialResponse){
+            const intern = new Intern(userResponse.employeeName, userResponse.employeeId, userResponse.employeeEmail, specialResponse.github)
+            employeeArray.push(Intern);
+            stopPrompt();
+         })
+    }
+    
+
+   }
+}
+// Function asking to user if they would like to stop creating employees
+
+function stopPrompt(){
+    inquirer.prompt {[
+        {
+            name: "stop",
+            type: "confirm",
+            message: "Would you like to stop adding employees?"
+        }
+    ]}.then(function(res){
+        if(res.stop){
+            const currentEmployeeData = render(employeeArray);
+            fs.writeFile(outputPath, currentEmployeeData, function(err){
+                if(err) throw err;
+            })
+        } else {
+            promptCommon();
+        }
+    }
+}
+
 
 
 // Write code to use inquirer to gather information about the development team members,
